49abc6e505878aa87ec1a2bacf862335ff7d90f3
Jhonatan David Fernandez Rosa <38236432+jhonatanfdez@users.noreply.github.com>
2025-09-01 16:10:49 -0400

    Update user.sh

--- user.sh
+++ user.sh
@@ -52,3 +52,26 @@ while IFS=: read -r user _ uid _ _ _ _; do
 
   printf "%-20s %-10s %-35s %-22s\n" "$user" "${passwd_s:-N/A}" "${shadow2:-N/A}" "$estatus"
 done < /etc/passwd
+
+
+
+
+
+
+
+printf "otro script"
+printf "=========="
+
+
+
+# Cruza /etc/passwd (para nombres) y /etc/shadow (estatus)
+while IFS=: read -r user _ uid _ _ _ _; do
+  shadow2=$(sudo awk -F: -v u="$user" '$1==u{print $2}' /etc/shadow)
+  # Interpretación simple
+  if [[ "$shadow2" =~ ^\$ ]]; then st="ACTIVO"
+  elif [[ -z "$shadow2" || "$shadow2" == "!" || "$shadow2" == "!!" || "$shadow2" == "*" || "$shadow2" == NP ]]; then st="INACTIVO"
+  else st="OTRO: $shadow2"; fi
+  printf "%-20s %-30s %-12s\n" "$user" "${shadow2:-N/A}" "$st"
+done < /etc/passwd
+
+
