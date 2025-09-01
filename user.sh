96cf7f2b8c5a8022afb8eef623b85450159dcb58
Jhonatan David Fernandez Rosa <38236432+jhonatanfdez@users.noreply.github.com>
2025-09-01 16:43:35 -0400

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,9 +1,40 @@
+
 #!/bin/bash
 # Script: estado_todos_usuarios_detallado.sh
 # Muestra TODOS los usuarios con:
 # - PASSWD_S (código crudo de `passwd -S`)
 # - SHADOW($2) (campo 2 de /etc/shadow)
 # - ESTATUS (interpretación)
+
+#17.2 | Recorre todos los usuarios locales (UID >= 1000 para excluir cuentas de sistema)
+printf "%-20s %-10s\n" "USUARIO" "ESTADO"
+
+echo "------------------------------------"
+ 
+for user in $(getent passwd | awk -F: '$3 >= 1 {print $1}'); do
+
+    estado=$(sudo passwd -S "$user" 2>/dev/null | awk '{print $2}')
+
+    case "$estado" in
+
+        P)  status="ACTIVO" ;;
+
+        L|LK) status="INACTIVO" ;;
+
+        NP) status="INACTIVO (SIN CONTRASEÑA)" ;;
+
+        *)  status="$estado" ;;
+
+    esac
+
+    printf "%-20s %-10s\n" "$user" "$status"
+
+done
+
+ 
+
+
+
 
 
 printf "Cambio"
@@ -20,5 +51,4 @@ while IFS=: read -r user _ uid _ _ _ _; do
   else st="OTRO: $shadow2"; fi
   printf "%-20s %-30s %-12s\n" "$user" "${shadow2:-N/A}" "$st"
 done < /etc/passwd
-
 
